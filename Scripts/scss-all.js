e617c7c768050a8327b4fe50b962bba7329050e9
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-02-23 02:02:47 +0100

    Linting and compiling scss files works

--- Scripts/scss-all.js
+++ Scripts/scss-all.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const stylelint = require('stylelint');
 
 console.log('args:', process.argv);
 
@@ -7,3 +8,6 @@ const nodejsExtensionRootDir = process.env.npm_config_local_prefix;
 const callerRootDir = path.join(nodejsExtensionRootDir, '..', '..');
 
 console.log('root:', callerRootDir);
+console.log('ENV :', process.env);
+
+// execute CLI commands: https://stackoverflow.com/a/37438131/177710
