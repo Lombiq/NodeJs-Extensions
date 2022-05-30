db85f5d247b521bef05f59e3990e9fd8ac711f0c
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-05-30 18:12:52 +0200

    Adding minification to the mix. Hurray!

---
+++ Lombiq.NodeJs.Extensions/scripts/minify.js
@@ -0,0 +1,43 @@
+/**
+ * @summary A wrapper script to allow minification per file as opposed to minification into a single output file.
+ */
+const fs = require('fs');
+const path = require('path');
+const exec = require('child_process').exec;
+
+// Get the target folder from the invocation.
+const arguments = process.argv.splice(2);
+
+if (arguments.length !== 1) {
+    throw Error('Please provide the folder to process as the only argument.');
+}
+
+const workingDir = arguments[0];
+
+// Go up two levels into the consumer project's root.
+process.chdir(workingDir);
+
+console.debug(`Minifying ${process.cwd()}...`);
+
+fs.readdir('.', (error, files) => {
+    files.forEach(file => {
+        if (!file.endsWith('.js') || file.endsWith('.min.js')) return;
+
+        const destination = path.basename(file, '.js') + '.min.js';
+
+        // Prefer the terser CLI for simplity over the API. See https://github.com/terser/terser for more information.
+        const command =
+            `terser "${file}" --output "${destination}" --compress --mangle --source-map "content=inline,url='${destination}'"`;
+
+        exec(command, (err, stdout, stderr) => {
+            if (err) {
+                console.error(`${file}: ${err}`);
+            }
+            else {
+                // Print the *entire* stdout and stderr (buffered).
+                stdout && console.log(`${file}: ${stdout}`);
+                stderr && console.log(`${file}: ${stderr}`);
+            }
+        });
+    });
+});
