36e7de329117ae207a18e01b7f9deb478ee14eed
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-06-08 12:19:22 +0200

    Clean up, restore functionality

--- Lombiq.NodeJs.Extensions/scripts/get-target.js
+++ Lombiq.NodeJs.Extensions/scripts/get-target.js
@@ -3,12 +3,6 @@
  * @description This has become necessary due to inconsistent, OS-dependent handling of the broken directory traversal
  *              via '..' in the shell used by pnpm.
  */
-function getSourceDir() {
-    const jsSourceKey = 'npm_config_target';
-    const sourceDir = process.env[jsSourceKey] || 'wwwroot/css';
-    return require('path').resolve(process.cwd(), '..', '..', sourceDir);
-}
+const targetDir = require('path').resolve(process.cwd(), '..', '..');
 
-const sourceDir = getSourceDir();
-
-console.log(require('fs').existsSync(sourceDir) ? sourceDir : 'NOK');
+process.stdout.write(targetDir);
