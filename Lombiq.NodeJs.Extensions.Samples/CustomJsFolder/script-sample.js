11845fc0938d373ff40c2ed77bced778657c4963
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-06-08 23:03:00 +0200

    Prevent linter violations from happening to fix build

--- Lombiq.NodeJs.Extensions.Samples/CustomJsFolder/script-sample.js
+++ Lombiq.NodeJs.Extensions.Samples/CustomJsFolder/script-sample.js
@@ -1,7 +1,9 @@
-// Unnamed function warning.
-(function () {
-    const thisIsAnUtterlyLongLine = 'A wonderfully long line to trigger a warning when the max line length is set to 120 in .eslintrc.json';
-    // console statement warning.
-    console.log(thisIsAnUtterlyLongLine);
+// Remove the "x" to trigger an "Unnamed function" warning.
+(function x() {
+    // Put into one line to trigger a custom rule in this project with a max line length of 120.
+    const thisIsAnUtterlyLongLine = 'A wonderfully long line to trigger a warning when the max line length is set to ' +
+        '120 in .eslintrc.json';
+    // Uncomment the next line to trigger "console statement" warning.
+    // console.log(thisIsAnUtterlyLongLine);
     return thisIsAnUtterlyLongLine;
 })();
