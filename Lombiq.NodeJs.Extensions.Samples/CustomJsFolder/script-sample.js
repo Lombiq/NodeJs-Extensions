1512279644ff96316125a552827301424aaaaf13
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-06-08 12:19:38 +0200

    Add comments to sample

--- Lombiq.NodeJs.Extensions.Samples/CustomJsFolder/script-sample.js
+++ Lombiq.NodeJs.Extensions.Samples/CustomJsFolder/script-sample.js
@@ -1,5 +1,7 @@
+// Unnamed function warning.
 (function () {
-    const thisIsAnUtterlyLongLine = 'A wonderfully long line to trigger a warning once you set the max line length to 120 in .eslintrc.json';
+    const thisIsAnUtterlyLongLine = 'A wonderfully long line to trigger a warning when the max line length is set to 120 in .eslintrc.json';
+    // console statement warning.
     console.log(thisIsAnUtterlyLongLine);
     return thisIsAnUtterlyLongLine;
 })();
