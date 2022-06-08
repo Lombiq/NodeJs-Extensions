a58bf61904dbb78332ec597eb98fe266d0c1a07b
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-06-08 10:39:17 +0200

    Generalize path retrieval, refactor JS scripts

--- Lombiq.NodeJs.Extensions/scripts/exists.js
+++
@@ -1,6 +0,0 @@
-/**
- * @summary A helper script to determine whether a given path exists.
- */
-const fs = require('fs');
-
-process.exit(fs.existsSync(process.argv[2]) ? 0 : 1);
--- Lombiq.NodeJs.Extensions/scripts/get-path.js
+++ Lombiq.NodeJs.Extensions/scripts/get-path.js
@@ -1,20 +1,39 @@
 /**
- * @summary A helper script to return the target directory to run our scripts in - the consumer project's JS folder.
- * @description This has become necessary due to inconsistent, OS-dependent handling of the broken directory traversal
- *              via '..' in the shell used by pnpm.
+ * @summary A helper script to return the respective directories to run our scripts in.
+ * @description Returns paths based on optional parameters and default fallback values. This script also ensures
+ *              OS-independent handling of the broken directory traversal via '..' in the shell used by pnpm.
  */
 const fs = require('fs');
 const path = require('path');
 
-const args = process.argv.splice(2);
-const type = process.args[0];
+const defaults = {
+    js_source: 'Assets/Scripts',
+    js_target: 'wwwroot/js',
+    scss_source: 'Assets/Styles',
+    scss_target: 'wwwroot/css',
+};
 
+const args = process.argv.splice(2);
+
+const type = args[0];
 if (type !== 'js' && type !== 'scss') {
-    throw Error('Please provide the folder to process as the only argument.');
+    throw Error('Please provide the type of files to process as the first argument: \'js\' or \'scss\'.');
 }
 
-const jsSourceKey = 'npm_config_js_source';
-const sourceDir = process.env[jsSourceKey] || 'Assets/Scripts';
-const sourcePath = path.resolve(process.cwd(), '..', '..', sourceDir);
+const location = args[1];
+if (location !== 'source' && location !== 'target') {
+    throw Error('Please provide the location to retrieve as the second argument: \'source\' or \'target\'.');
+}
 
-process.stdout.write(fs.existsSync(sourcePath) ? sourcePath : '!');
+const configuration = `${type}_${location}`;
+
+// The npm_config_* environment variables are automatically created when passing arguments to scripts; e.g.
+// --js-source=my/path will lead to the existence of npm_config_js_source=my/path.
+const envKey = `npm_config_${configuration}`;
+const effectiveDir = process.env[envKey] || defaults[configuration];
+
+// We traverse two levels up, because the Node.js Extensions npm package is located at ./node_modules/nodejs-extensions.
+const effectivePath = path.resolve(process.cwd(), '..', '..', effectiveDir);
+
+// Writing the existing path to stdout lets us consume it at the call site. If source does not exist, we return '!'.
+process.stdout.write(location === 'target' || fs.existsSync(effectivePath) ? effectivePath : '!');
