d9caea18249a7f3316255a215d544b820baa6b66
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-06-23 02:50:22 +0200

    Get all files included under Linux, too

--- Lombiq.NodeJs.Extensions.Samples/CustomJsFolder/sample-script.js
+++ Lombiq.NodeJs.Extensions.Samples/CustomJsFolder/sample-script.js
@@ -2,8 +2,8 @@
 (function x() {
     // Put into one line to trigger a custom rule in this project with a max line length of 120.
     const veryLongLine = 'A wonderfully long line to trigger a warning when the max line length is set to 120 in ' +
-        '.eslintrc.json';
+        '.eslintrc.json'
     // Uncomment the next line to trigger "console statement" warning.
     // console.log(veryLongLine);
-    return veryLongLine;
+    return veryLongLine; 
 })();
