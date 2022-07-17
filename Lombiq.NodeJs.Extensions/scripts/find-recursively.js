b9f6db53f8fcb652aa12d2001ad79fd08ee00e87
Dávid El-Saig <david.el-saig@lombiq.com>
2022-07-17 22:24:00 +0200

    Move markdown-to-msbuild.js here, set up scripts.

---
+++ Lombiq.NodeJs.Extensions/scripts/find-recursively.js
@@ -0,0 +1,36 @@
+﻿/**
+ * @summary A helper function to recursively search for files (matched with regular expressions) while not traversing
+ *          the excluded directories.
+ * @description Returns an array of absolute paths inside the provided startPath that match the includeFiles and isn't
+ *              located in a directory that match the excludeDirectories.
+ */
+const fs = require('fs');
+const path = require('path');
+
+function findRecursively(startPath, includeFiles, excludeDirectories) {
+    const results = [];
+
+    function addIfInclude(parentPath, fileEntity) {
+        if (includeFiles.some((include) => fileEntity.name.match(include))) {
+            results.push(path.resolve(parentPath, fileEntity.name))
+        }
+    }
+
+    function findInner(here) {
+        const segments = here.split(/[\\\/]+/);
+        const name = segments[segments.length - 1];
+
+        if (excludeDirectories.some((exclude) => name.match(exclude))) return;
+
+        fs.readdirSync(here, { withFileTypes: true }).forEach((child) =>
+            child.isDirectory()
+                ? findInner(path.resolve(here, child.name))
+                : addIfInclude(here, child));
+    }
+
+    findInner(path.resolve(startPath));
+
+    return results;
+}
+
+module.exports = findRecursively;
---
+++ Lombiq.NodeJs.Extensions/scripts/markdown-to-msbuild.js
@@ -0,0 +1,50 @@
+﻿const findRecursively = require('./find-recursively');
+const markdownlint = require('markdownlint');
+const process = require('process');
+
+const files = findRecursively(
+    process.argv.length > 2 ? process.argv[2] : '.',
+    [/\.md$/i],
+    [/^node_modules$/, /^\.git$/, /^obj$/, /^bin$/]);
+const config = {
+    default: true,
+    MD013: false,
+    MD033: {
+        allowed_elements: [
+            // A special element in GitHub to indicate a keyboard key. Other Markdown formatters that don't support it will
+            // safely ignore the tags and render the content as inline text without adverse effects.
+            'kbd',
+        ]
+    }
+};
+
+try {
+    const results = markdownlint.sync({ files, config });
+
+    Object.keys(results).forEach((fileName) => {
+        results[fileName].forEach((warning) => {
+            const line = warning.lineNumber;
+            const column = (Array.isArray(warning.errorRange) && !isNaN(warning.errorRange[0]))
+                ? warning.errorRange[0]
+                : 1;
+            const [code, name] = Array.isArray(warning.ruleNames)
+                ? warning.ruleNames
+                : ['WARN', 'unknown-warning'];
+
+            // License files don't need title.
+            if (fileName.toLowerCase().endsWith('license.md') && code === 'MD041') return;
+
+            let message = `${name ? name : code}: ${warning.ruleDescription.trim()}`;
+            if (!message.endsWith('.')) message += '.';
+            if (warning.fixInfo) message += ' An automatic fix is available with markdownlint-cli.';
+            if (warning.ruleInformation) message += ' Rule information: ' + warning.ruleInformation;
+
+            process.stdout.write(`${fileName}(${line},${column}): warning ${code}: ${message}\n`);
+        });
+    });
+}
+catch (error) {
+    const code = error.code ? error.code : 'ERROR';
+    process.stderr.write(`${error.path}(1,1): error ${code}: ${error.toString()}\n`);
+    process.exit(1);
+}
