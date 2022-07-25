345fe43dc8fb9029485f68b07cf1ec9f0725e86d
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-07-25 23:41:43 +0200

    Remove the source dir from the copied file path
    - add optional pattern property to asset group definition

--- Lombiq.NodeJs.Extensions/scripts/copy-assets.js
+++ Lombiq.NodeJs.Extensions/scripts/copy-assets.js
@@ -4,14 +4,15 @@
  *              the consuming project named "assets-to-copy.json" which has the following format:
  *              [
  *                {
- *                  "sources": [ "Assets/Images/*.*" ],
+ *                  "sources": [ "Assets/Images" ],
  *                  "target": "wwwroot/images"
  *                },
  *                {
  *                  "sources": [
- *                    "node_modules/marvelous/** /*"
- *                    "node_modules/wonderful/** /*"
+ *                    "node_modules/marvelous/dist"
+ *                    "node_modules/wonderful/bin"
  *                  ],
+ *                  "pattern": "*",
  *                  "target": "wwwroot"
  *                }
  *                // more groups, if needed
@@ -20,21 +21,43 @@
 const fs = require('fs');
 const path = require('path');
 const copyfiles = require('copyfiles');
-const assetFileName = 'assets-to-copy.json';
+
+const verbose = false;
+const assetsFileName = 'assets-to-copy.json';
+const filePattern = '**/*';
 
 // Change to consuming project's directory.
 process.chdir('..');
 process.chdir('..');
 
-const assetsFile = path.resolve(process.cwd(), assetFileName);
+const assetsFile = path.resolve(process.cwd(), assetsFileName);
 
 if (fs.existsSync(assetsFile)) {
     const assetsConfig = JSON.parse(fs.readFileSync(assetsFile, 'utf8'));
     for (let i = 0; i < assetsConfig.length; i++) {
         const assetsGroup = assetsConfig[i];
-        const paths = [...assetsGroup.sources, assetsGroup.target];
 
-        // https://www.npmjs.com/package/copyfiles#programic-api
-        copyfiles(paths, {}, () => {});
+        for (let j = 0; j < assetsGroup.sources.length; j++) {
+            // Normalize the relative path to the directory to remove trailing slashes and straighten out any anomalies.
+            const directoryToCopy = path.relative('.', assetsGroup.sources[j]);
+
+            fs.access(directoryToCopy, (err) => {
+                if (err) {
+                    process.stderr.write(`The directory "${directoryToCopy}" cannot be accessed to copy files from.`);
+                }
+                else {
+                    const pathPattern = path.join(directoryToCopy, assetsGroup.pattern || filePattern);
+                    const sourceAndTargetPaths = [pathPattern, assetsGroup.target];
+
+                    // We want to copy all files matched by the given pattern into the target folder mirroring the
+                    // source folder structure. This is done by removing the source folder path from the beginning which
+                    // "copyfiles" does using the "up" option.
+                    const depth = directoryToCopy.split(/[\\/]/).filter().length;
+
+                    // https://github.com/calvinmetcalf/copyfiles#programic-api
+                    copyfiles(sourceAndTargetPaths, { verbose: verbose, up: depth }, () => {});
+                }
+            });
+        }
     }
 }
