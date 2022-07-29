c41c28c90d0b055cd25eab371b6ec7c430a977b2
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-07-29 10:28:10 +0200

    Make styles pipeline optional

---
+++ Lombiq.NodeJs.Extensions/scripts/skip-styles.js
@@ -0,0 +1,19 @@
+/**
+ * @summary A helper script to determine whether to skip running the styles pipeline.
+ * @description Returns 0 if the styles pipeline should be skipped, else 1.
+ */
+
+const { access } = require('node:fs/promises');
+const path = require('path');
+
+(async function main() {
+    try {
+        const stylesSourceDirectory = path.resolve('..', '..', process.env.SCSS_SOURCE);
+        await access(stylesSourceDirectory);
+        process.exit(1);
+    }
+    catch (_) {
+        process.stdout.write('Skipping styles');
+        process.exit(0);
+    }
+})();
