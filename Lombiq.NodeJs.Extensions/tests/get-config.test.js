7b5d74a3bdb455fd31ed0e63683f1d64a9c7a63c
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-09-05 14:05:37 +0200

    Add tests to our module scripts using jest

---
+++ Lombiq.NodeJs.Extensions/tests/get-config.test.js
@@ -0,0 +1,52 @@
+/* eslint-env jest */
+
+const fs = require('fs');
+const path = require('path');
+const getConfig = require('../scripts/get-config');
+const validate = require('../scripts/validate-config');
+
+const verbose = false;
+let packageJson = null;
+
+jest.mock('fs');
+jest.mock('path');
+jest.mock('../scripts/validate-config');
+fs.readFileSync.mockImplementation(() => packageJson);
+path.resolve.mockImplementation(() => 'string value');
+validate.mockResolvedValue(true);
+
+test('having no configuration is valid', () => {
+    packageJson = '{}';
+    const config = getConfig({ verbose });
+    expect(config).toHaveProperty('scripts');
+    expect(config).toHaveProperty('styles');
+});
+
+test('empty configuration is valid', () => {
+    packageJson = '{ "nodejsExtensions": {} }';
+    const config = getConfig({ verbose });
+    expect(config).toHaveProperty('scripts');
+    expect(config).toHaveProperty('styles');
+});
+
+test('empty configuration return default values', () => {
+    packageJson = '{ "nodejsExtensions": {} }';
+    const config = getConfig({ verbose });
+    expect(config.scripts.source).toBe('Assets/Scripts');
+    expect(config.scripts.target).toBe('wwwroot/js');
+    expect(config.styles.source).toBe('Assets/Styles');
+    expect(config.styles.target).toBe('wwwroot/css');
+});
+
+test('assetToCopy configuration without pattern will have default pattern', () => {
+    packageJson = '{ "nodejsExtensions": { "assetsToCopy": [{ "sources": [], "target": "" }] } }';
+    const assetConfig = getConfig({ verbose }).assetsToCopy[0];
+    expect(assetConfig.pattern).toBe('**/*');
+});
+
+test('configuration returns custom scripts paths', () => {
+    packageJson = '{ "nodejsExtensions": { "scripts": { "source": "CustomJs", "target": "CustomWww" } } }';
+    const config = getConfig({ verbose }).scripts;
+    expect(config.source).toBe('CustomJs');
+    expect(config.target).toBe('CustomWww');
+});
