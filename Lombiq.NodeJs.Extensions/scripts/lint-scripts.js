14bd925e18bc2b844984d6cfc198b2256b17075c
Dávid El-Saig <david.el-saig@lombiq.com>
2022-10-05 07:55:50 +0200

    Clean up lint-scripts.

--- Lombiq.NodeJs.Extensions/scripts/lint-scripts.js
+++ Lombiq.NodeJs.Extensions/scripts/lint-scripts.js
@@ -1,9 +1,6 @@
-﻿const fs = require('fs');
-const path = require('path');
+﻿const path = require('path');
 const { ESLint } = require("eslint");
-const { handleWarningObject, handleErrorMessage, handleWarningMessage } = require('./handle-error');
-
-process.stdout.write('FORMATTER!1\n');
+const { handleWarningObject, handleErrorMessage } = require('./handle-error');
 
 function formatResult(result) {
     result.messages?.forEach((warning) =>
@@ -23,20 +20,15 @@ const options = {
     errorOnUnmatchedPattern: false,
 };
 
-process.stdout.write('FORMATTER!3\n');
 (async function main() {
     // 1. Create an instance.
-    process.stdout.write('FORMATTER!4\n');
     const eslint = new ESLint(options);
 
     // 2. Lint files.
-    process.stdout.write('FORMATTER!5\n');
     const results = await eslint.lintFiles('./**/*.js');
-    process.stdout.write('FORMATTER: ' + JSON.stringify(results) + '!\n');
     if (!Array.isArray(results) || results.length === 0) return;
 
     // 3. Format the results.
-    process.stdout.write('FORMATTER!6\n');
     results.forEach(formatResult);
 })().catch((error) => {
     handleErrorMessage(error);
