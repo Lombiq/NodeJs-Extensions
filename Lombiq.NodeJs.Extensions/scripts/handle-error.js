181d85b48bd07adf5b337e33b161600ea9e8d578
Dávid El-Saig <david.el-saig@lombiq.com>
2022-10-21 18:17:27 +0200

    Add `@summary` tags.

--- Lombiq.NodeJs.Extensions/scripts/handle-error.js
+++ Lombiq.NodeJs.Extensions/scripts/handle-error.js
@@ -22,7 +22,7 @@ function handleErrorObjectInner(error, type, defaultCode) {
 }
 
 /**
- * Displays an MSBuild error from an object.
+ * @summary Displays an MSBuild error from an object.
  * @param error The object describing the error. Must have a `message` property or `toString` implementation. It may
  *              also have `code`, `path`, `line`, `column` and `stack` properties.
  * @param defaultCode If `error.code` is not available then this value is used.
@@ -32,7 +32,7 @@ function handleErrorObject(error, defaultCode = 'ERROR') {
 }
 
 /**
- * Displays an MSBuild warning from an object.
+ * @summary Displays an MSBuild warning from an object.
  * @param error The object describing the warning. Must have a `message` property or `toString` implementation. It may
  *              also have `code`, `path`, `line`, `column` and `stack` properties.
  * @param defaultCode If `error.code` is not available then this value is used.
@@ -42,13 +42,14 @@ function handleWarningObject(error, defaultCode = 'WARN') {
 }
 
 /**
- * Displays an MSBuild error from a value.
+ * @summary Displays an MSBuild error from a value.
  * @param message This value is converted to `string` before it's displayed.
  */
 function handleErrorMessage(message) { handleErrorObject({ message: message.toString() }); }
 
+
 /**
- * Displays an MSBuild warning from a value.
+ * @summary Displays an MSBuild warning from a value.
  * @param message This value is converted to `string` before it's displayed.
  */
 function handleWarningMessage(message) { handleWarningObject({ message: message.toString() }); }
