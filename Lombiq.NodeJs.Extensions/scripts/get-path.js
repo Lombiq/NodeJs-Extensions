9192b4e77a553b609dc37feb1ba6d859d0a7208c
Oliver Friedrich <oliver.friedrich@lombiq.com>
2023-01-02 14:45:22 +0100

    Code style fixes

--- Lombiq.NodeJs.Extensions/scripts/get-path.js
+++ Lombiq.NodeJs.Extensions/scripts/get-path.js
@@ -11,9 +11,9 @@ const getConfig = require('./get-config');
 const verbose = false;
 const solutionFolderMarker = '_solution_';
 const extensionToTypeMap = {
-    'js': 'scripts',
-    'md': 'markdown',
-    'scss': 'styles',
+    js: 'scripts',
+    md: 'markdown',
+    scss: 'styles',
 };
 const log = (message) => {
     if (verbose) process.stderr.write(`# ${message}\n`);
@@ -54,7 +54,7 @@ function getRelativePath() {
     }
 
     log(`effectiveDir: ${effectiveDir}`);
-    
+
     // We traverse two levels up, because the Node.js Extensions NPM package is located at
     // ./node_modules/nodejs-extensions.
     const effectivePath = path.resolve(initialDirectory, effectiveDir);
