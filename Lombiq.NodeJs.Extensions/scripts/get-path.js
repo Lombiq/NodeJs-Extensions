6138b31855ce24256bf36ac8390d61a5e1003b55
Oliver Friedrich <oliver.friedrich@lombiq.com>
2023-01-02 14:47:03 +0100

    Turn on verbosity for internal get-path script

--- Lombiq.NodeJs.Extensions/scripts/get-path.js
+++ Lombiq.NodeJs.Extensions/scripts/get-path.js
@@ -8,7 +8,7 @@ const path = require('path');
 const process = require('process');
 const getConfig = require('./get-config');
 
-const verbose = false;
+const verbose = true;
 const solutionFolderMarker = '_solution_';
 const extensionToTypeMap = {
     js: 'scripts',
