2004cca71983a3f0bb7ed83114ed49a67dcbcfab
Oliver Friedrich <oliver.friedrich@lombiq.com>
2023-02-08 20:17:00 +0100

    Revert "Rename script, use add instead of install, rename vars"
    This reverts commit 0b3894ba2f5edecfa89202a4903e2eb0ba9bb9fb.

--- Lombiq.NodeJs.Extensions/scripts/install-dev-dependencies.js
+++ Lombiq.NodeJs.Extensions/scripts/install-dev-dependencies.js
@@ -5,21 +5,21 @@ const { execSync } = require('child_process');
 const { handleErrorObject } = require('./handle-error');
 
 const configPath = path.resolve(__dirname, '..', 'package.json');
-const nxDependencies = JSON.parse(fs.readFileSync(configPath)).dependencies;
-const eslintPackages = Object
-    .entries(nxDependencies)
+const dependencies = JSON.parse(fs.readFileSync(configPath)).dependencies;
+const packages = Object
+    .entries(dependencies)
     .filter(([name]) => name.startsWith('eslint'))
-    .map(([name, version]) => `${name}@"${version}"`)
+    .map(([name, version]) => `${name}@${version}`)
     .join(' ');
 
 // Ensure each of the default development dependencies are installed. If they already are, this finishes very quickly.
 // It is necessary, because by default NPM and PNPM don't install the dependencies of local packages, see here:
 // https://docs.npmjs.com/cli/v9/configuring-npm/package-json#local-paths
 try {
-    execSync('pnpm add --save-dev ' + eslintPackages);
+    execSync('pnpm install --save-dev ' + packages);
 }
 catch (exception) {
-    process.stderr.write('Failed to install packages: ' + eslintPackages);
+    process.stderr.write('Failed to install packages: ' + packages);
     handleErrorObject(exception);
     process.exit(1);
 }
