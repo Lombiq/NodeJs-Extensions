6bd5de99c1d2a0dc234578d0c6307da39153694c
Oliver Friedrich <oliver.friedrich@lombiq.com>
2023-03-01 10:25:47 +0100

    Retry command inside of Invoke-Sequentially, fail with non-zero exit code via throw

--- Lombiq.NodeJs.Extensions/build/Invoke-Sequentially.ps1
+++ Lombiq.NodeJs.Extensions/build/Invoke-Sequentially.ps1
@@ -18,39 +18,59 @@ $timeoutMs = 20000
 $sleepMs = 100
 $maxTries = $timeoutMs / $sleepMs
 $stream = $null
+$errorCount = 0
+$maxErrors = 5
+$done = $false
+$hasError = $false
 
-# Try to get an exclusive lock on a lock file to guarantee only one thread will execute the given $Command at any given time.
-for ($currentTry = 0; $currentTry -lt $maxTries -and $null -eq $stream; $currentTry++)
+# Try to get an exclusive lock on a file to guarantee only one thread will execute the given $Command at any given time.
+for ($currentTry = 0; -not $done -and $currentTry -lt $maxTries -and $errorCount -lt $maxErrors; $currentTry++)
 {
     try
     {
-        $stream = [System.IO.File]::Open($LockFilePath, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read, [System.IO.FileShare]::None)
+        $stream = [System.IO.File]::Open(
+            $LockFilePath, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read, [System.IO.FileShare]::None)
         Write-Output "$MessagePrefix Acquired lock after $currentTry iterations."
-        Invoke-Expression $Command
+        # Adding "fail" here will throw an exception in case $Command itself fails, even without an exception. This is
+        # to jump into the catch block and count errors before failing the script.
+        Invoke-Expression "$Command || fail"
+        # We only end up here if the above line did not throw any exception, which means it ran successfully.
+        $done = $true
     }
     catch [System.IO.IOException]
     {
         # These are expected to happen, keep going.
-        Start-Sleep -Milliseconds $sleepMs
     }
     catch
     {
-        # When an unexpected exception is thrown, fail the script.
-        Write-Output $PSItem.Exception
-        exit 1
+        $errorCount++
+        $currentTry = -1
+        # Only print the error if it's not because of calling "fail".
+        if ($PSItem.Exception.CommandName -ne 'fail')
+        {
+            Write-Output $PSItem.Exception
+        }
     }
     finally
     {
         if ($null -ne $stream)
         {
             $stream.Dispose()
+            $stream = $null
             Write-Output "$MessagePrefix Released lock."
         }
+
+        Start-Sleep -Milliseconds $sleepMs
     }
 }
 
-if ($null -eq $stream)
+# When an unexpected exception is thrown for too many times, fail the script.
+if ($errorCount -ge $maxErrors)
 {
-    Write-Error "$MessagePrefix Did not acquire the necessary lock to run '$Command' within $($maxTries * $sleepMs)ms. Please try again."
-    exit 1
+    throw "$MessagePrefix Failed to run '$Command' successfully within $errorCount tries. Please try again."
+}
+
+if (-not $done)
+{
+    throw "$MessagePrefix Failed to run '$Command' within $($maxTries * $sleepMs)ms. Please try again."
 }
