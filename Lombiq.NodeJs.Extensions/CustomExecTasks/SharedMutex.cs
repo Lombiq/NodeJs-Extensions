7eb828df5ab1412f6287f2e929454a5aabe665f7
Oliver Friedrich <oliver.friedrich@lombiq.com>
2023-04-13 00:27:43 +0200

    Fix fast disposal of mutex object

--- Lombiq.NodeJs.Extensions/CustomExecTasks/SharedMutex.cs
+++ Lombiq.NodeJs.Extensions/CustomExecTasks/SharedMutex.cs
@@ -17,22 +17,25 @@ public class SharedMutex
         _timeout = timeout;
     }
 
-    public bool Execute(Func<bool> functionToExecute, Action<string, object[]> logWait, Action<string, object[]> logError)
+    public bool Execute(
+        Func<bool> functionToExecute, Action<string, object[]> logWait = null, Action<string, object[]> logError = null)
     {
         var count = 1;
         var stopwatch = Stopwatch.StartNew();
-        using var mutex = new Mutex(initiallyOwned: false, _mutexName);
         while (stopwatch.Elapsed <= _timeout)
         {
-            if (mutex.WaitOne(0))
+            using (var mutex = new Mutex(initiallyOwned: false, _mutexName))
             {
-                // Release the mutex asap because we don't need it for execution. We only needed it to verify that it is
-                // currently not "locked", i.e. in exclusive usage.
-                mutex.ReleaseMutex();
+                if (mutex.WaitOne(0))
+                {
+                    // Release the mutex asap because we don't need it for execution. We only needed it to verify that
+                    // it is currently not "locked", i.e. in exclusive usage.
+                    mutex.ReleaseMutex();
 
-                logWait?.Invoke("Acquired shared access to {0} in {1}.", new object[] { _mutexName, stopwatch.Elapsed });
+                    logWait?.Invoke("Acquired shared access to {0} in {1}.", new object[] { _mutexName, stopwatch.Elapsed });
 
-                return functionToExecute();
+                    return functionToExecute();
+                }
             }
 
             logWait?.Invoke("#{0} Waiting for shared access to {1}.", new object[] { count++, _mutexName });
