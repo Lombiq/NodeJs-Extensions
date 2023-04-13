f3973e70cb90e48d6bd958a6b5a4c60b6c6ddcb4
0liver <0liver@users.noreply.github.com>
2023-04-14 00:45:02 +0200

    Remove explicit enum values
    Co-authored-by: József Horváth <jozsef.horvath@lombiq.com>

--- Lombiq.NodeJs.Extensions/CustomExecTasks/MutexAccess.cs
+++ Lombiq.NodeJs.Extensions/CustomExecTasks/MutexAccess.cs
@@ -2,7 +2,7 @@ namespace Lombiq.NodeJs.Extensions.CustomExecTasks;
 
 public enum MutexAccess
 {
-    Undefined = 0,
-    Exclusive = 1,
-    Shared = 2,
+    Undefined,
+    Exclusive,
+    Shared,
 }
