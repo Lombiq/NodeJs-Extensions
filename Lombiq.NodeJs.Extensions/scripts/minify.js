5d6132db70c6039b2d506626990fa9b1981f3704
Dávid El-Saig <david.el-saig@lombiq.com>
2023-10-07 16:11:32 +0200

    typo

--- Lombiq.NodeJs.Extensions/scripts/minify.js
+++ Lombiq.NodeJs.Extensions/scripts/minify.js
@@ -18,7 +18,7 @@ if (args.length !== 1) {
 }
 
 // Switch to the desired working directory.
-process.chdir(pargs[0]);
+process.chdir(args[0]);
 
 // We only walk inside this path so it doesn't matter if it's symlinked or not (no need for get-cwd.js).
 const workingDir = process.cwd();
