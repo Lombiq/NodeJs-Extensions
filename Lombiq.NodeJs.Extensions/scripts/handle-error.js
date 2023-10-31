43ec6acf4cc6b61c21bb37421852fa97be9a956f
Sára El-Saig <sara.el-saig@lombiq.com>
2023-10-31 01:13:18 +0100

    Revert "Revert "OSOE-430: Support a single NE instance per solution on Linux, too, in Lombiq.NodeJs.Extensions""
    This reverts commit 2deabf5310a90f6e54d3017e86712c36c03daa16.

--- Lombiq.NodeJs.Extensions/scripts/handle-error.js
+++ Lombiq.NodeJs.Extensions/scripts/handle-error.js
@@ -7,7 +7,11 @@ const os = require('os');
 // Treat this dependency as optional because it's not available everywhere.
 let chalk;
 try {
+    /* eslint-disable import/no-unresolved -- ESLint does not know where to find external modules. */
+    /* eslint-disable global-require -- Optional dependency. */
     chalk = require('chalk');
+    /* eslint-enable global-require */
+    /* eslint-enable import/no-unresolved */
 }
 catch {
     chalk = false;
@@ -83,8 +87,14 @@ function handlePromiseRejectionAsError(promise, panic = false) {
         });
 }
 
+function handleErrorObjectAndExit(error, defaultCode = 'ERROR') {
+    handleErrorObject(error, defaultCode);
+    process.exit(1);
+}
+
 module.exports = {
     handleErrorObject,
+    handleErrorObjectAndExit,
     handleWarningObject,
     handleErrorMessage,
     handleWarningMessage,
