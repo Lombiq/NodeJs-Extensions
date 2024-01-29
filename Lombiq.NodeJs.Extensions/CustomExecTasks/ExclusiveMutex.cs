ba3e490e6d82898c2a8b4ff69fb8df4089a6dc3c
Roland Márkus <roland.markus@lombiq.com>
2024-01-29 14:08:47 +0100

    Removing fields.

--- Lombiq.NodeJs.Extensions/CustomExecTasks/ExclusiveMutex.cs
+++ Lombiq.NodeJs.Extensions/CustomExecTasks/ExclusiveMutex.cs
@@ -6,9 +6,6 @@ namespace Lombiq.NodeJs.Extensions.CustomExecTasks;
 
 public class ExclusiveMutex(string mutexName, TimeSpan timeout)
 {
-    private readonly string _mutexName = mutexName;
-    private readonly TimeSpan _timeout = timeout;
-
     public int RetryIntervalMs { get; set; } = 100;
     public int WaitTimeMs { get; set; } = 1000;
 
@@ -17,9 +14,9 @@ public class ExclusiveMutex(string mutexName, TimeSpan timeout)
     {
         var count = 1;
         var stopwatch = Stopwatch.StartNew();
-        while (stopwatch.Elapsed <= _timeout)
+        while (stopwatch.Elapsed <= timeout)
         {
-            using (var mutex = new Mutex(initiallyOwned: false, _mutexName, out var createdNew))
+            using (var mutex = new Mutex(initiallyOwned: false, mutexName, out var createdNew))
             {
                 // We only try to acquire the mutex in case it was freshly created, because that means that no other
                 // processes are currently using it, including in a shared way.
@@ -28,7 +25,7 @@ public class ExclusiveMutex(string mutexName, TimeSpan timeout)
                     try
                     {
                         logWait?.Invoke(
-                            "Acquired exclusive access to {0} after {1}.", [_mutexName, stopwatch.Elapsed]);
+                            "Acquired exclusive access to {0} after {1}.", [mutexName, stopwatch.Elapsed]);
                         return functionToExecute();
                     }
                     finally
@@ -38,11 +35,11 @@ public class ExclusiveMutex(string mutexName, TimeSpan timeout)
                 }
             }
 
-            logWait?.Invoke("#{0} Waiting for exclusive access to {1}.", [count++, _mutexName]);
+            logWait?.Invoke("#{0} Waiting for exclusive access to {1}.", [count++, mutexName]);
             Thread.Sleep(RetryIntervalMs);
         }
 
-        logError?.Invoke("Failed to acquire exclusive access {0} in {1}.", [_mutexName, _timeout]);
+        logError?.Invoke("Failed to acquire exclusive access {0} in {1}.", [mutexName, timeout]);
         return false;
     }
 }
