abd32e5a810ad893adda4af9ec50d6099a283c2a
Roland Márkus <roland.markus@lombiq.com>
2024-02-14 14:25:02 +0100

    Using LangVersion 7.3 in NetStandard2.0 NodeJsExtensions.

--- Lombiq.NodeJs.Extensions/CustomExecTasks/ExecWithMutex.cs
+++ Lombiq.NodeJs.Extensions/CustomExecTasks/ExecWithMutex.cs
@@ -3,69 +3,77 @@ using Microsoft.Build.Tasks;
 using System;
 using System.Threading;
 
-namespace Lombiq.NodeJs.Extensions.CustomExecTasks;
-
-/// <summary>
-/// An Exec task, wrapped around a critical section.
-/// </summary>
-/// <remarks>
-/// <para>
-/// We want to synchronize many shared readers and exclusive writers. Here's how to do that using a <see cref="Mutex"/>.
-/// </para>
-/// <list type="number">
-/// <item>
-/// <description>
-/// Any reader process simply creates a Mutex instance without calling WaitOne() for the time of execution. This will
-/// signal to the writer, that at least one reader is already relying on that Mutex. At the same, this approach still
-/// allows other readers to proceed in parallel. To assure that no writer is currently using the Mutex, any reader will
-/// call WaitOne(0) on it. If that call fails, it means that a writer is currently using the Mutex, and we have to wait.
-/// </description>
-/// </item>
-/// <item>
-/// <description>
-/// The writer process first creates a Mutex instance and checks whether it was created just now. If yes, it will
-/// process to "lock" the Mutex by calling WaitOne() on it. If not, it means that a reader is currently processing and
-/// the writer needs to retry (after a short wait time).
-/// </description>
-/// </item>
-/// </list>
-/// </remarks>
-public class ExecWithMutex : Exec
+namespace Lombiq.NodeJs.Extensions.CustomExecTasks
 {
-    public MutexAccess MutexAccessToUse =>
-        Enum.TryParse(Access, ignoreCase: true, out MutexAccess access) ? access : MutexAccess.Undefined;
-
-    public TimeSpan TimeoutSpan => TimeSpan.FromSeconds(TimeoutSeconds);
-
     /// <summary>
-    /// Gets or sets the mutex name.
+    /// An Exec task, wrapped around a critical section.
     /// </summary>
-    [Required]
-    public string MutexName { get; set; }
-
-    /// <summary>
-    /// Gets or sets the <see cref="MutexAccess"/> level to use on this Mutex.
-    /// </summary>
-    [Required]
-    public string Access { get; set; }
-
-    /// <summary>
-    /// Gets or sets the maximum number of seconds that any thread should wait for the given mutex.
-    /// </summary>
-    [Required]
-    public int TimeoutSeconds { get; set; }
-
-    /// <inheritdoc />
-    public override bool Execute() => MutexAccessToUse switch
+    /// <remarks>
+    /// <para>
+    /// We want to synchronize many shared readers and exclusive writers. Here's how to do that using a <see cref="Mutex"/>.
+    /// </para>
+    /// <list type="number">
+    /// <item>
+    /// <description>
+    /// Any reader process simply creates a Mutex instance without calling WaitOne() for the time of execution. This will
+    /// signal to the writer, that at least one reader is already relying on that Mutex. At the same, this approach still
+    /// allows other readers to proceed in parallel. To assure that no writer is currently using the Mutex, any reader will
+    /// call WaitOne(0) on it. If that call fails, it means that a writer is currently using the Mutex, and we have to wait.
+    /// </description>
+    /// </item>
+    /// <item>
+    /// <description>
+    /// The writer process first creates a Mutex instance and checks whether it was created just now. If yes, it will
+    /// process to "lock" the Mutex by calling WaitOne() on it. If not, it means that a reader is currently processing and
+    /// the writer needs to retry (after a short wait time).
+    /// </description>
+    /// </item>
+    /// </list>
+    /// </remarks>
+    public class ExecWithMutex : Exec
     {
-        MutexAccess.Shared => new SharedMutex(MutexName, TimeoutSpan)
-            .Execute(base.Execute, Log.LogMessage, Log.LogError),
+        public MutexAccess MutexAccessToUse =>
+            Enum.TryParse(Access, ignoreCase: true, out MutexAccess access) ? access : MutexAccess.Undefined;
 
-        MutexAccess.Exclusive => new ExclusiveMutex(MutexName, TimeoutSpan)
-            .Execute(base.Execute, Log.LogMessage, Log.LogError),
+        public TimeSpan TimeoutSpan => TimeSpan.FromSeconds(TimeoutSeconds);
 
-        _ => throw new ArgumentException(
-            $"The \"{nameof(Access)}\" attribute on the {nameof(ExecWithMutex)} task needs to be set to either " +
-            $"\"{nameof(MutexAccess.Shared)}\" or \"{nameof(MutexAccess.Exclusive)}\"!"),
-    };
+        /// <summary>
+        /// Gets or sets the mutex name.
+        /// </summary>
+        [Required]
+        public string MutexName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="MutexAccess"/> level to use on this Mutex.
+        /// </summary>
+        [Required]
+        public string Access { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum number of seconds that any thread should wait for the given mutex.
+        /// </summary>
+        [Required]
+        public int TimeoutSeconds { get; set; }
+
+        /// <inheritdoc />
+        public override bool Execute()
+        {
+            switch (MutexAccessToUse)
+            {
+                case MutexAccess.Shared:
+                    return new SharedMutex(MutexName, TimeoutSpan)
+                        .Execute(base.Execute, Log.LogMessage, Log.LogError);
+
+                case MutexAccess.Exclusive:
+                    return new ExclusiveMutex(MutexName, TimeoutSpan)
+                        .Execute(base.Execute, Log.LogMessage, Log.LogError);
+                case MutexAccess.Undefined:
+                    return false;
+                default:
+                    throw new ArgumentException(
+                        $"The \"{nameof(Access)}\" attribute on the {nameof(ExecWithMutex)} task needs to be set to either " +
+                        $"\"{nameof(MutexAccess.Shared)}\" or \"{nameof(MutexAccess.Exclusive)}\"!");
+            }
+        }
+    }
 }
--- Lombiq.NodeJs.Extensions/CustomExecTasks/SharedMutex.cs
+++ Lombiq.NodeJs.Extensions/CustomExecTasks/SharedMutex.cs
@@ -2,48 +2,49 @@ using System;
 using System.Diagnostics;
 using System.Threading;
 
-namespace Lombiq.NodeJs.Extensions.CustomExecTasks;
-
-public class SharedMutex
+namespace Lombiq.NodeJs.Extensions.CustomExecTasks
 {
-    private readonly string _mutexName;
-    private readonly TimeSpan _timeout;
-
-    public int RetryIntervalMs { get; set; } = 100;
-
-    public SharedMutex(string mutexName, TimeSpan timeout)
+    public class SharedMutex
     {
-        _mutexName = mutexName;
-        _timeout = timeout;
-    }
+        private readonly string _mutexName;
+        private readonly TimeSpan _timeout;
 
-    public bool Execute(
-        Func<bool> functionToExecute, Action<string, object[]> logWait = null, Action<string, object[]> logError = null)
-    {
-        var count = 1;
-        var stopwatch = Stopwatch.StartNew();
-        while (stopwatch.Elapsed <= _timeout)
+        public int RetryIntervalMs { get; set; } = 100;
+
+        public SharedMutex(string mutexName, TimeSpan timeout)
         {
-            using (var mutex = new Mutex(initiallyOwned: false, _mutexName))
-            {
-                if (mutex.WaitOne(0))
-                {
-                    // Release the mutex asap because we don't need it for execution. We only needed it to verify that
-                    // it is currently not "locked", i.e. in exclusive usage.
-                    mutex.ReleaseMutex();
-
-                    logWait?.Invoke("Acquired shared access to {0} in {1}.", [_mutexName, stopwatch.Elapsed]);
-
-                    return functionToExecute();
-                }
-            }
-
-            logWait?.Invoke("#{0} Waiting for shared access to {1}.", [count++, _mutexName]);
-
-            Thread.Sleep(RetryIntervalMs);
+            _mutexName = mutexName;
+            _timeout = timeout;
         }
 
-        logError?.Invoke("Failed to acquire {0} in {1}.", [_mutexName, _timeout]);
-        return false;
+        public bool Execute(
+            Func<bool> functionToExecute, Action<string, object[]> logWait = null, Action<string, object[]> logError = null)
+        {
+            var count = 1;
+            var stopwatch = Stopwatch.StartNew();
+            while (stopwatch.Elapsed <= _timeout)
+            {
+                using (var mutex = new Mutex(initiallyOwned: false, _mutexName))
+                {
+                    if (mutex.WaitOne(0))
+                    {
+                        // Release the mutex asap because we don't need it for execution. We only needed it to verify that
+                        // it is currently not "locked", i.e. in exclusive usage.
+                        mutex.ReleaseMutex();
+
+                        logWait?.Invoke("Acquired shared access to {0} in {1}.", new object[] { _mutexName, stopwatch.Elapsed });
+
+                        return functionToExecute();
+                    }
+                }
+
+                logWait?.Invoke("#{0} Waiting for shared access to {1}.", new object[] { count++, _mutexName });
+
+                Thread.Sleep(RetryIntervalMs);
+            }
+
+            logError?.Invoke("Failed to acquire {0} in {1}.", new object[] { _mutexName, _timeout });
+            return false;
+        }
     }
 }
