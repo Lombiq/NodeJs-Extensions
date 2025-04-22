b6c6f20a4369a5b5cff2ea5237f969d52389d86f
Sára El-Saig <sara.el-saig@lombiq.com>
2025-04-22 23:35:36 +0200

    Fix pwsh code styling.

--- .github/actions/lint/Invoke-NodeJsLinter.ps1
+++ .github/actions/lint/Invoke-NodeJsLinter.ps1
@@ -4,25 +4,28 @@ param (
     [string] $Type,
     [string] $Paths)
 
-function Test-NoPath($Paths) { -not (Test-Path -Path $Paths | Where-Object { $PsItem }) }
+function Test-NoPath($Paths) { -not (Test-Path -Path $Paths | Where-Object { $PSItem }) }
 
 function Install-NodeJsExtensions($Path)
 {
     Set-Location $Path
 
-    if (Test-NoPath -Paths package.json,package.json5,package.yaml) {
+    if (Test-NoPath -Paths package.json, package.json5, package.yaml)
+    {
         Copy-Item $LibraryPath/config/consumer/package.project.json package.json
     }
 
-    if (Test-NoPath -Paths .eslintrc,.eslintrc.js) {
+    if (Test-NoPath -Paths .eslintrc, .eslintrc.js)
+    {
         Copy-Item $LibraryPath/config/consumer/.eslintrc.project.js .eslintrc.js
     }
 
-    if (Test-NoPath -Paths .prettierrc,.prettierrc.js) {
+    if (Test-NoPath -Paths .prettierrc, .prettierrc.js)
+    {
         Copy-Item $LibraryPath/config/consumer/.prettierrc.project.js .prettierrc.js
     }
 
-    $StyleLintPaths = @(
+    $styleLintPaths = @(
         '.stylelintrc'
         '.stylelintrc.js'
         '.stylelintrc.mjs'
@@ -34,7 +37,8 @@ function Install-NodeJsExtensions($Path)
         'stylelint.config.mjs'
         'stylelint.config.cjs'
     )
-    if (Test-NoPath -Paths $StyleLintPaths) {
+    if (Test-NoPath -Paths $styleLintPaths)
+    {
         Copy-Item $LibraryPath/config/consumer/.stylelintrc.project.js .stylelintrc.js
     }
 
@@ -42,12 +46,11 @@ function Install-NodeJsExtensions($Path)
     pnpm install @Packages
 }
 
-function Invoke-NodeJsExtensions($Type, $Paths) {
-    if ($Paths.Trim()) {
-        $Paths.Split(',') | ForEach-Object { $PsItem.Trim() } | Get-Item | ForEach-Object {
-            Install-NodeJsExtensions -Path $PsItem
-            Invoke-NodeJsExtensions -Type $Type
-        }
+if ($Paths.Trim())
+{
+    $Paths.Split(',') | ForEach-Object { $PSItem.Trim() } | Get-Item | ForEach-Object {
+        Install-NodeJsExtensions -Path $PSItem
+        Invoke-NodeJsExtensions -Type $Type
     }
-    npm explore nodejs-extensions -- pnpm "lint:$Type"
 }
+npm explore nodejs-extensions -- pnpm "lint:$Type"
