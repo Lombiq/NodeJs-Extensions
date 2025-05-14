5473e5a641bde5ebbb6d62625a209670927ff6e1
Sára El-Saig <sara.el-saig@lombiq.com>
2025-05-14 16:04:48 +0200

    Update rimraf invocation.

--- Lombiq.NodeJs.Extensions/scripts/clean-assets.js
+++ Lombiq.NodeJs.Extensions/scripts/clean-assets.js
@@ -6,7 +6,7 @@
 const path = require('path');
 
 /* eslint-disable-next-line import/no-unresolved -- ESLint does not know where to find external modules. */
-const rimraf = require('util').promisify(require('rimraf'));
+const { rimraf } = require('rimraf');
 
 const getConfig = require('./get-config');
 const getProjectDirectory = require('./get-project-directory');
