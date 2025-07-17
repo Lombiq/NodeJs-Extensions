eea9ae5c1518a0b6d00f07ff91535025bf6de19b
Krisztián Németh <krisztian.nemeth@lombiq.com>
2025-07-17 12:06:47 +0200

    Exclude third party folders

--- Lombiq.NodeJs.Extensions/scripts/lint-markdown.js
+++ Lombiq.NodeJs.Extensions/scripts/lint-markdown.js
@@ -44,7 +44,7 @@ function getMarkdownPaths() {
     return findRecursively(
         rootDirectory,
         [/\.md$/i],
-        [/^node_modules$/, /^\.git$/, /^\.vs$/, /^\.vscode$/, /^\.idea$/, /^obj$/, /^bin$/, /^wwwroot$/]);
+        [/^node_modules$/, /^\.git$/, /^\.vs$/, /^\.vscode$/, /^\.idea$/, /^obj$/, /^bin$/, /^wwwroot$/, /^ThirdParty$/]);
 }
 
 function handleError(error) {
