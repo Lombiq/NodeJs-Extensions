985f99dfd5c62d28d1c1b27115ab92150dec12fb
Sára El-Saig <sara.el-saig@lombiq.com>
2025-07-29 15:35:18 +0200

    Enforce sourceType for mjs and cjs.

--- Lombiq.NodeJs.Extensions/scripts/lint-code.js
+++ Lombiq.NodeJs.Extensions/scripts/lint-code.js
@@ -1,9 +1,29 @@
 import { ESLint } from 'eslint';
 
 import { formatter } from './eslint-msbuild-formatter.js';
+import { getProjectDirectory } from './get-project-directory.js';
+
+function getSourceType(id) {
+    const parts = `${id}`.replace(/\?.*/, '').split('.');
+    const extension = parts[parts.length - 1].toLowerCase();
+
+    return extension === 'mjs' ? 'module' :
+           extension === 'cjs' ? 'commonjs' :
+           undefined;
+}
 
 export async function lintCode(code, id, firstRow = 1) {
-    const eslint = new ESLint({ errorOnUnmatchedPattern: false });
+    const options = {
+        cwd: getProjectDirectory(),
+        overrideConfig: {
+            languageOptions: {
+                sourceType: getSourceType(id),
+                ecmaVersion: 2020,
+            }
+        }
+    };
+
+    const eslint = new ESLint(options);
     const results = await eslint.lintText(code, { filePath: id });
 
     if (!Array.isArray(results) || results.length === 0) return;
