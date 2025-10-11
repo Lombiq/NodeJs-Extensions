9719cedc0379e3518c762109ba81bda1a8b1a637
Sára El-Saig <sara.el-saig@lombiq.com>
2025-10-11 20:12:29 +0200

    Fix extends value.

--- Lombiq.NodeJs.Extensions/eslint.config.cjs
+++ Lombiq.NodeJs.Extensions/eslint.config.cjs
@@ -3,7 +3,7 @@ const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintrc = compat.config({
     // The following path may have to be adjusted to your directory structure.
-    extends: 'Lombiq.NodeJs.Extensions/config/.eslintrc.lombiq-base.js',
+    extends: 'config/.eslintrc.lombiq-base.js',
 
     // Add custom rules and overrides here.
     rules: {
