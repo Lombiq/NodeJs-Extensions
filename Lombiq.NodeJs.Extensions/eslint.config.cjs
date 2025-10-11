398b9953f75c5baa79a3946ed32cb2a09728784e
Sára El-Saig <sara.el-saig@lombiq.com>
2025-10-11 20:14:38 +0200

    Use relative path.

--- Lombiq.NodeJs.Extensions/eslint.config.cjs
+++ Lombiq.NodeJs.Extensions/eslint.config.cjs
@@ -3,7 +3,7 @@ const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintrc = compat.config({
     // The following path may have to be adjusted to your directory structure.
-    extends: 'config/.eslintrc.lombiq-base.js',
+    extends: './config/.eslintrc.lombiq-base.js',
 
     // Add custom rules and overrides here.
     rules: {
