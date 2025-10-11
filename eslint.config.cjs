6719dde90f01a4b2d7260f35192afaa0cd72f6c9
Sára El-Saig <sara.el-saig@lombiq.com>
2025-10-11 20:07:13 +0200

    Add eslint.config.cjs.

---
+++ eslint.config.cjs
@@ -0,0 +1,13 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+const eslintrc = compat.config({
+    // The following path may have to be adjusted to your directory structure.
+    extends: 'Lombiq.NodeJs.Extensions/config/.eslintrc.lombiq-base.js',
+
+    // Add custom rules and overrides here.
+    rules: {
+    },
+});
+
+module.exports = [ ...eslintrc ]
