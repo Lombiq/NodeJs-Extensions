5576aebe3059bdb1e5a81cd195f303946930a299
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-10-22 02:07:25 +0200

    NuGet publish

---
+++ Lombiq.NodeJs.Extensions.Samples.NuGet/eslint.config.cjs
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+let eslintrc = compat.config({
+    // The following path may have to be adjusted to your directory structure.
+    extends: './node_modules/nodejs-extensions/config/.eslintrc.lombiq-base.js',
+
+    // Add custom rules and overrides here.
+    rules: {
+    },
+});
+
+if (fs.existsSync('./.eslintrc.js')) {
+    // eslint-disable-next-line global-require, import/extensions -- Special conditional require.
+    eslintrc = require('./.eslintrc.js');
+}
+
+module.exports = [...eslintrc];
